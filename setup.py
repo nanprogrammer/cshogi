16ee01b226cc54b2dcbf284647d4e9ee6b13964e
nanprogrammer <43568274+nanprogrammer@users.noreply.github.com>
2023-01-19 19:21:04 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ ext_modules = [
          "src/bitboard.cpp", "src/common.cpp", "src/generateMoves.cpp", "src/hand.cpp", "src/init.cpp", "src/move.cpp", "src/mt64bit.cpp", "src/position.cpp", "src/search.cpp", "src/square.cpp", "src/usi.cpp", "src/book.cpp", "src/mate.cpp", "src/dfpn.cpp"],
         language='c++',
         include_dirs = ["src"],
-        define_macros=[('HAVE_SSE4', None), ('HAVE_SSE42', None), ('HAVE_AVX2', None)]),
+        define_macros=[(None), (None), (None)]),
     Extension('cshogi.gym_shogi.envs.shogi_env',
         ['cshogi/gym_shogi/envs/shogi_env.pyx'],
         language='c++'),
